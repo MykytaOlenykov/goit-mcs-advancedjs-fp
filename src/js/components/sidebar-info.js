639932ac42a8a35740c2b1e3130e713182ed2f38
Liliana <liliana.ubisoft@gmail.com>
2024-08-28 14:54:39 +0100

    working js, css for tablet with problem

--- src/js/components/sidebar-info.js
+++ src/js/components/sidebar-info.js
@@ -1,23 +1,49 @@
-// import { getQuote } from '..\services\api.js'
+// import { getQuote } from '../services/api.js';
+import axios from 'axios';
 
-// function sidebarInfo() {
-//     let storedQuote = localStorage.getItem('quote');
-//     const today = new Date();
-//     if (!storedQuote || storedQuote === 'undefined' || storedQuote === 'null') {
-//         getQuote().then((data) => {
-            
-//             data["date"] = today.getDate();
-//             localStorage.setItem('quote', JSON.stringify(data));
-//         });
-//     }
-//     storedQuote = localStorage.getItem('quote');
-//     const dataQuote = JSON.parse(storedQuote);
-    
-//     const sidebarInfo = document.querySelector('.p_quote_text');
-//     sidebarInfo.innerHTML = dataQuote["quote"];
+async function getQuote() {
+    try {
+        const response = await axios.get('https://your-energy.b.goit.study/api/quote');
+        return response.data; 
+    } catch (error) {
+        console.error('Error fetching quote', error);
+        throw error; 
+    }
+}
 
-//     const sidebarInfoAuthor = document.querySelector('.p_quote_author');
-//     sidebarInfoAuthor.innerHTML = dataQuote["author"];
-// }
+function sidebarInfo() {
+    let storedQuote = localStorage.getItem('quote');
+    const today = new Date().getDate();
 
+    if (!storedQuote) {
+        getQuote().then((data) => {
+            console.log(data);
+            data.date = today; // Добавляем дату в объект цитаты
+            localStorage.setItem('quote', JSON.stringify(data));
+            updateQuote(data);
+        });
+    } else {
+        const dataQuote = JSON.parse(storedQuote);
+        
+        // Проверка, что цитата не устарела
+        if (dataQuote.date !== today) {
+            getQuote().then((data) => {
+                data.date = today;
+                localStorage.setItem('quote', JSON.stringify(data));
+                updateQuote(data);
+            });
+        } else {
+            updateQuote(dataQuote);
+        }
+    }
+}
 
+function updateQuote(dataQuote) {
+    const sidebarInfo = document.querySelector('.p_quote_text');
+    sidebarInfo.innerHTML = dataQuote.quote;
+
+    const sidebarInfoAuthor = document.querySelector('.p_quote_author');
+    sidebarInfoAuthor.innerHTML = dataQuote.author;
+}
+
+sidebarInfo();
