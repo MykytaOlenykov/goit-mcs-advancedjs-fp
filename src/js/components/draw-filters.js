605ee117da809bc56979d1c0302fc53be9bf05f1
Gregory Kucherkov <gregorykucherkov@Gregorys-Laptop.N4>
2024-08-30 20:20:34 +0300

    workout_cards

--- src/js/components/draw-filters.js
+++ src/js/components/draw-filters.js
@@ -25,28 +25,43 @@ export default function draw_filters(cards) {
   cardList.innerHTML = markup;
 }
 
-
 export function draw_exercies(keyword) {
   const markup_exercies = keyword
-  .map(
-    key => 
-      `<li class="exercises__name">
-          <span>Workout</span>
-          ${key.rating}
+    .map(
+      key =>
+        `<li class="exercises__name">
+       <div class="exercises__name-wraper-1">
+        <span class="exercises__name-tag">Workout</span>
+        <span class="exercises__name-rating">${key.rating}</span>
+        <button class="exercises__name-btn" type="button" data-modal-open="">Start</button>
+        
+          <svg class="exercises__name-icon" width="16" height="16" style="stroke: black;">
+            <use href="./assets/icons/icons-sprite.svg#arrow"></use>
+          </svg>
+        
+        
+        </div>
 
-          <button class="exercises__name-btn" type="button" data-modal-open="">Start</button>
-          <h3>${key.name}</h3>
-          <ul>
-          <li>BurnedCalories${key.burnedCalories}</li>
-          <li>Body part${key.bodyPart}</li>
-          <li>Target${key.target}</li>
-          
-          
-          </ul>
-            
+        <div class="exercises__name-h3-wraper">
+        <h3 class="exercises__name-h3">${key.name}</h3>
+        <div class="exersize__h3-icon-wraper">
+          <svg class="exersize__h3-icon" width="18" height="18">
+            <use href="../assets/icons/icons-sprite.svg#runner"></use>
+          </svg>
+        </div>
+        </div>
+
+        <ul class="exercises__name-props" >
+            <li class="exercises__name-item" >BurnedCalories:<span class="exercises__name-clr">${key.burnedCalories}</span></li>
+            <li class="exercises__name-item">Body part:<span class="exercises__name-clr">${key.bodyPart}</span></li>
+            <li class="exercises__name-item">Target:<span class="exercises__name-clr">${key.target}</span></li>
+        </ul>
         </li>
         `
-  ).join('');
+    )
+    .join('');
+
+  cardList.classList.add('exercises__cards-wrkt');  
 
   cardList.innerHTML = markup_exercies;
 }
