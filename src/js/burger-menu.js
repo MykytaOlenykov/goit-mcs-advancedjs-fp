58471b00b42161afd70fba95615ba79c42c299d4
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2024-08-30 19:18:12 +0300

    add header

---
+++ src/js/burger-menu.js
@@ -0,0 +1,36 @@
+import { toggleModal } from './common.js';
+
+const refs = {
+  openBurgerBtn: document.querySelector('[data-burger-open]'),
+  closeBurgerBtn: document.querySelector('[data-burger-close]'),
+  navLinks: document.querySelectorAll('[data-nav-link]'),
+  burgerMenu: document.querySelector('[data-burger]'),
+  burgerMenuContent: document.querySelector('[data-burger-content]'),
+  body: document.querySelector('body'),
+};
+
+refs.openBurgerBtn.addEventListener('click', () => toggleModal(refs.burgerMenu));
+refs.closeBurgerBtn.addEventListener('click', () => toggleModal(refs.burgerMenu));
+refs.navLinks.forEach(link => {
+  link.addEventListener('click', () => toggleModal(refs.burgerMenu));
+});
+
+// Outside click
+window.addEventListener('click', e => {
+  if (
+    refs.burgerMenu.classList.contains('is-hidden') ||
+    refs.burgerMenuContent.contains(e.target) ||
+    refs.openBurgerBtn.contains(e.target)
+  ) {
+    return;
+  }
+
+  toggleModal(refs.burgerMenu)
+});
+
+window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  if (!refs.burgerMenu.classList.contains('is-hidden')) {
+    toggleModal(refs.burgerMenu)
+  }
+});
