eeadd94f732bc7a6f56d7fef490a9b6228d0ce1d
ArthurMykhailiuk <artur.myhajlyuk@volo.global>
2024-08-31 16:29:20 +0300

    fix comments

--- src/js/components/burger-menu.js
+++ src/js/components/burger-menu.js
@@ -1,42 +1,28 @@
-
-const toggleModal = (modalSelector) => {
-    modalSelector.classList.toggle('is-hidden');
-  
-    handleBodyOverflow(modalSelector);
-    handleZIndex(modalSelector);
-  };
+const toggleModal = modalSelector => {
+  modalSelector.classList.toggle('is-hidden');
+};
 
 const refs = {
   openBurgerBtn: document.querySelector('[data-burger-open]'),
   closeBurgerBtn: document.querySelector('[data-burger-close]'),
-  navLinks: document.querySelectorAll('[data-nav-link]'),
   burgerMenu: document.querySelector('[data-burger]'),
-  burgerMenuContent: document.querySelector('[data-burger-content]'),
   body: document.querySelector('body'),
 };
 
-refs.openBurgerBtn.addEventListener('click', () => toggleModal(refs.burgerMenu));
-refs.closeBurgerBtn.addEventListener('click', () => toggleModal(refs.burgerMenu));
-refs.navLinks.forEach(link => {
-  link.addEventListener('click', () => toggleModal(refs.burgerMenu));
-});
+refs.openBurgerBtn.addEventListener('click', () =>
+  toggleModal(refs.burgerMenu)
+);
+refs.closeBurgerBtn.addEventListener('click', () =>
+  toggleModal(refs.burgerMenu)
+);
 
-// Outside click
 window.addEventListener('click', e => {
   if (
     refs.burgerMenu.classList.contains('is-hidden') ||
-    refs.burgerMenuContent.contains(e.target) ||
     refs.openBurgerBtn.contains(e.target)
   ) {
     return;
   }
 
-  toggleModal(refs.burgerMenu)
-});
-
-window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
-  if (!e.matches) return;
-  if (!refs.burgerMenu.classList.contains('is-hidden')) {
-    toggleModal(refs.burgerMenu)
-  }
+  toggleModal(refs.burgerMenu);
 });
